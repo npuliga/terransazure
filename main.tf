8ff8c582a6bc6393471b0db6ca2bf3859f3fc622
Naga <npuliga@gmail.com>
2017-05-31 13:40:25 -0400

    removed client secret

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
-subscription_id = "d6ab6f2d-bcfb-4d29-abf9-a73b26577726"
-tenant_id = "b0e5bb12-4165-446c-8ea8-3c4a83012fad"
-client_id = "6d93a675-e168-401e-8933-3515456da273"
-client_secret = "fd794532-d5aa-4a68-83e6-14e029e90e3d"
 }
 
 resource "azurerm_resource_group" "dev" {
@@ -110,7 +106,7 @@ storage_image_reference {
   provisioner "local-exec" {
    command = "timeout 300"
   }
-  
+
   provisioner "file" {
     source      = "ansible/apache2.yaml"
     destination = "/home/adminuser"
