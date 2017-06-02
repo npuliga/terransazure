129b1154ac9308f7559d4408ffe9c906def29232
Naga <npuliga@gmail.com>
2017-06-02 04:04:33 -0400

    updated sgacc

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "azurerm_network_interface" "dev" {
 }
 
 resource "azurerm_storage_account" "dev" {
-  name                = "nagap2018"
+  name                = "nagap2019"
   resource_group_name = "${azurerm_resource_group.dev.name}"
   location            = "${var.region}"
   account_type        = "Standard_LRS"
